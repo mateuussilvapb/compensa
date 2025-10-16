c169578b4b39f4b00b530b105397b4eba8999cb0
Mateus Dias <mateuussilvapb@gmail.com>
2025-10-16 07:51:33 -0300

    ClientId google

--- src/app/config/auth/auth.config.ts
+++ src/app/config/auth/auth.config.ts
@@ -3,7 +3,7 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 export const authConfig: AuthConfig = {
   issuer: 'https://accounts.google.com',
   redirectUri: globalThis.location.origin,
-  clientId: '',
+  clientId: '667273061879-qlu6o9e4orrq7g213l289vcnrltu6iid.apps.googleusercontent.com',
   scope: 'openid profile email',
   strictDiscoveryDocumentValidation: false,
 }
