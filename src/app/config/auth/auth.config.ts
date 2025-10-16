f04c3fc9596f2b18c0efe557e3458f808ac02adb
Mateus Dias <mateuussilvapb@gmail.com>
2025-10-16 08:00:34 -0300

    ajustes redirectUri

--- src/app/config/auth/auth.config.ts
+++ src/app/config/auth/auth.config.ts
@@ -2,7 +2,7 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const authConfig: AuthConfig = {
   issuer: 'https://accounts.google.com',
-  redirectUri: globalThis.location.origin,
+  redirectUri: window.location.origin,
   clientId: '667273061879-qlu6o9e4orrq7g213l289vcnrltu6iid.apps.googleusercontent.com',
   scope: 'openid profile email',
   strictDiscoveryDocumentValidation: false,
