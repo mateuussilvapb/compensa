7c8797b3ab598a14600156cfdbe67976a233fafd
Mateus Dias <mateuussilvapb@gmail.com>
2025-11-08 15:01:46 -0300

    Permissão de execução no script

---
+++ scripts/set-config.sh
@@ -0,0 +1,10 @@
+echo "{
+  \"firebaseConfig\": {
+    \"apiKey\": \"${NG_APP_FIREBASE_API_KEY}\",
+    \"authDomain\": \"${NG_APP_FIREBASE_AUTH_DOMAIN}\",
+    \"projectId\": \"${NG_APP_FIREBASE_PROJECT_ID}\",
+    \"storageBucket\": \"${NG_APP_FIREBASE_STORAGE_BUCKET}\",
+    \"messagingSenderId\": \"${NG_APP_FIREBASE_MESSAGING_SENDER_ID}\",
+    \"appId\": \"${NG_APP_FIREBASE_APP_ID}\"
+  }
+}" > dist/compensa/src/assets/config/config.json
